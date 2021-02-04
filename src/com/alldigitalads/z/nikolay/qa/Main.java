56642daa62584f5ed5d74ca45a9f069739dd8fa0
Nikolay <nikola.zakusilo20071991@gmail.com>
2021-02-04 20:17:31 +0200

    Initial commit

---
+++ src/com/alldigitalads/z/nikolay/qa/Main.java
@@ -0,0 +1,11 @@
+package com.alldigitalads.z.nikolay.qa;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello wold");
+        System.out.println(2 + 2 * 2);
+        System.out.println("Hi \n WOLD");
+        System.out.println("Hello wold = " +  (2 + 2 * 2) + ";");
+        System.out.println("Hello kitty");
+    }
+}
